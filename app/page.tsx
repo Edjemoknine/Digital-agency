5adbc4ce0a5eb53ac8650f389b6438ef908ba149
Moknine Elhadj <mokninemoknine999@gmail.com>
2024-03-16 18:38:42 +0100

    add Contact and Book a Call Pages

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Shopify from "@/components/shared/Shopify";
 import Services from "@/components/shared/Services";
 import { AccordionDemo } from "@/components/shared/Faq";
 import { useRef } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const ProjectRef = useRef<HTMLDivElement>(null);
@@ -44,7 +45,7 @@ export default function Home() {
           <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
             <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-              Border Magic
+              <Link href={"/book"}>Book Call</Link>
             </span>
           </button>
         </div>
